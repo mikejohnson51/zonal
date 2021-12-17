05a25780368b6d8f3dc6dc324d480fa42739d9c1
mikejohnson51 <jmj00@ucsb.edu>
2021-12-16 18:52:54 -0700

    attempt to merge execute cat into execute

--- R/execute.R
+++ R/execute.R
@@ -19,7 +19,6 @@
   w
 }
 
-
 #' Get Mode
 #' @param x vector of values
 #' @return mode value
@@ -36,7 +35,7 @@ getmode <- function(x) {
 #' @param na.rm Should NA values be removed?
 #' @return mode value
 #' @export
-#' 
+
 gm_mean = function(x, na.rm=TRUE){
   if(any(x  < 0)){
     x = x[x >= 0]
@@ -51,19 +50,81 @@ gm_mean = function(x, na.rm=TRUE){
 #' @param geom sf object of aggregation units 
 #' @param ID the name of the column providing the unique identified of each geom
 #' @param w a precomputed weighting grid produced with `weighting_grid`,
-#' @param FUN an optional function or character vector, as described below
+#' @param FUN an optional function or character vector, as described in details
+#' @param rcl a data.frame with columns names `from` and `to`. 
+#' `From` should be the categorical values used in the raster, 
+#' while `to` should be the categorical names to use in the output table headings.
+#' If left NULL (default) the headings will not be altered.
 #' @param join should output be joined to input geom by the supplied ID (geom cannot be NULL)
-#' @return data.table
+#' @return data.table or sf object
 #' @export
-#' @importFrom data.table getDTthreads setDTthreads
+#' @details The are current 6 built in summary functions:
+#' \itemize{
+#'  \item{"mean"}{The mean value of the coverage data}
+#'  \item{"mode"}{The most frequent of the coverage values}
+#'  \item{"max"}{The maximum of the coverage values}
+#'  \item{"min"}{The minimum of the coverage values}
+#'  \item{"gm_mean"}{The geometric mean of the coverage values}
+#'  \item{"percent"}{The percentage of each covered value (categorical data)}
+#' }
+#' @importFrom data.table `:=`
 
-execute_zonal    = function(file = NULL, 
+# execute_zonal    = function(file = NULL, 
+#                             geom = NULL, 
+#                             ID = NULL,
+#                             FUN = "mean",
+#                             w = NULL,
+#                             join = TRUE) {
+#   .SD <-  NULL
+#   
+#   if(join & is.null(geom)){
+#     warning("Can't `join` to NULL geom")
+#     join = FALSE
+#   }
+#   
+#   w       = .find_w(file, geom, ID, w)
+#   w_names = c("grid_id", "w")
+#   ID      = names(w$weight_map)[!names(w$weight_map) %in% w_names]
+# 
+#   dt   = .zonal_io(file, w)
+#   cols = names(dt)[!names(dt) %in% c(ID, w_names)]
+#   
+#   if(FUN == 'mean'){
+#     FUN = function(x, w) { sum((x * w), na.rm = TRUE)  / sum(w, na.rm = TRUE)}
+#   } else if(FUN == "max"){
+#     FUN = function(x, w) { suppressWarnings(max(x, na.rm = TRUE)) }
+#   } else if(FUN == "min"){
+#     FUN = function(x, w) { suppressWarnings(min(x, na.rm = TRUE)) }
+#   } else if(FUN == "mode"){
+#     FUN = function(x, w) { suppressWarnings(getmode(x)) }
+#   } else if(FUN == "gm_mean"){
+#     FUN = function(x, w) { suppressWarnings(gm_mean(x)) }
+#   } else {
+#     stop("FUN not valid...")
+#   }
+#   
+#   threds = getDTthreads()
+#   setDTthreads(0)
+#     exe = dt[, lapply(.SD, FUN = FUN, w = w), keyby = eval(ID), .SDcols = cols]
+#   setDTthreads(threds)
+# 
+#   if(join){
+#     merge(geom, exe, by = ID)
+#   } else {
+#     exe
+#   }
+#   
+# }
+
+execute_zonal  = function(file = NULL, 
                             geom = NULL, 
                             ID = NULL,
                             FUN = "mean",
                             w = NULL,
+                            rcl = NULL,
                             join = TRUE) {
-  .SD <-  NULL
+  
+  . <- .SD <- frac_total <- NULL
   
   if(join & is.null(geom)){
     warning("Can't `join` to NULL geom")
@@ -73,28 +134,39 @@ execute_zonal    = function(file = NULL,
   w       = .find_w(file, geom, ID, w)
   w_names = c("grid_id", "w")
   ID      = names(w$weight_map)[!names(w$weight_map) %in% w_names]
-
+  
   dt   = .zonal_io(file, w)
   cols = names(dt)[!names(dt) %in% c(ID, w_names)]
   
-  if(FUN == 'mean'){
-    FUN = function(x, w) { sum((x * w), na.rm = TRUE)  / sum(w, na.rm = TRUE)}
-  } else if(FUN == "max"){
-    FUN = function(x, w) { suppressWarnings(max(x, na.rm = TRUE)) }
-  } else if(FUN == "min"){
-    FUN = function(x, w) { suppressWarnings(min(x, na.rm = TRUE)) }
-  } else if(FUN == "mode"){
-    FUN = function(x, w) { suppressWarnings(getmode(x)) }
-  } else if(FUN == "gm_mean"){
-    FUN = function(x, w) { suppressWarnings(gm_mean(x)) }
+
+  if(FUN == "freq"){
+      dt[, frac_total := (w / sum(w, na.rm = TRUE)), by = c(ID)]
+    
+      dt = dt[, .(freq = sum(frac_total, na.rm = TRUE)), by = c(ID, cols)]
+      
+      if(!is.null(rcl)){ 
+        dt[[cols]] = rcl$to[match(dt[[cols]], rcl$from)] 
+      }
+      
+      exe = setnames(dt, c(ID, "value", "percentage"))
   } else {
-    stop("FUN not valid...")
-  }
   
-  threds = getDTthreads()
-  setDTthreads(0)
+    if(FUN == 'mean'){
+      FUN = function(x, w) { sum((x * w), na.rm = TRUE)  / sum(w, na.rm = TRUE)}
+    } else if(FUN == "max"){
+      FUN = function(x, w) { suppressWarnings(max(x, na.rm = TRUE)) }
+    } else if(FUN == "min"){
+      FUN = function(x, w) { suppressWarnings(min(x, na.rm = TRUE)) }
+    } else if(FUN == "mode"){
+      FUN = function(x, w) { suppressWarnings(getmode(x)) }
+    } else if(FUN == "gm_mean"){
+      FUN = function(x, w) { suppressWarnings(gm_mean(x)) }
+    } else {
+      stop("FUN not valid...")
+    }
+    
     exe = dt[, lapply(.SD, FUN = FUN, w = w), keyby = eval(ID), .SDcols = cols]
-  setDTthreads(threds)
+  }
 
   if(join){
     merge(geom, exe, by = ID)
@@ -102,5 +174,9 @@ execute_zonal    = function(file = NULL,
     exe
   }
 }
+
+
+
+
 
 
