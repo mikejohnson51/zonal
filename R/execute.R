f2ca71b92efc16111b66a80fdafec6b5f3eab385
mikejohnson51 <jmj00@ucsb.edu>
2022-03-03 17:06:58 -0700

    add list execution close #13, and improve nameing close #8

--- R/execute.R
+++ R/execute.R
@@ -1,3 +1,57 @@
+#' Internal Exectute Single Summary
+#' @param file SpatRast object
+#' @param w weight file
+#' @param w_name weight names
+#' @param name variable name
+#' @param FUN summary function
+#' @param rcl reclassification matrix
+#' @param ID geometry ID
+#' @return data.table
+
+single_file_execute = function(file, w, w_names, name,  FUN, rcl, ID){
+  
+  dt   = .zonal_io(file, w)
+  cols = names(dt)[!names(dt) %in% c(ID, w_names)]
+  
+  if(!is.null(name)){
+    names = gsub("V", paste0(name, "_"), cols)
+  } else {
+    names = cols
+  }
+  
+  if(FUN == "freq"){
+    dt[, frac_total := (w / sum(w, na.rm = TRUE)), by = c(ID)]
+    
+    dt = dt[, .(freq = sum(frac_total, na.rm = TRUE)), by = c(ID, cols)]
+    
+    if(!is.null(rcl)){ 
+      dt[[cols]] = rcl$to[match(dt[[cols]], rcl$from)] 
+    }
+    
+    exe = setnames(dt, c(ID, "value", "percentage"))
+  } else {
+    
+    if(FUN == 'mean'){
+      FUN = function(x, w) { sum((x * w), na.rm = TRUE)  / sum(w, na.rm = TRUE)}
+    } else if(FUN == "max"){
+      FUN = function(x, w) { suppressWarnings(max(x, na.rm = TRUE)) }
+    } else if(FUN == "min"){
+      FUN = function(x, w) { suppressWarnings(min(x, na.rm = TRUE)) }
+    } else if(FUN == "mode"){
+      FUN = function(x, w) { suppressWarnings(getmode(x)) }
+    } else if(FUN == "gm_mean"){
+      FUN = function(x, w) { suppressWarnings(gm_mean(x)) }
+    } else {
+      stop("FUN not valid...")
+    }
+    
+    exe = dt[, lapply(.SD, FUN = FUN, w = w), keyby = eval(ID), .SDcols = cols]
+    exe = setnames(exe, c(ID, names))
+  }
+  
+  exe
+}
+
 .getExtension <- function(file){ 
   ext <- strsplit(basename(file), split="\\.")[[1]]
   return(ext[length(ext)])
@@ -19,22 +73,10 @@
   w
 }
 
-#' Get Mode
-#' @param x vector of values
-#' @return mode value
-#' @export
-
 getmode <- function(x) {
   y <- unique(x)
   y[which.max(tabulate(match(x, y)))]
 }
-
-#' Get geometric mean
-#' @description Vectorized and suited to handle NAs and negative values
-#' @param x vector of values
-#' @param na.rm Should NA values be removed?
-#' @return mode value
-#' @export
 
 gm_mean = function(x, na.rm=TRUE){
   if(any(x  < 0)){
@@ -68,105 +110,53 @@ gm_mean = function(x, na.rm=TRUE){
 #'  \item{"percent"}{The percentage of each covered value (categorical data)}
 #' }
 #' @importFrom data.table `:=`
-
-# execute_zonal    = function(file = NULL, 
-#                             geom = NULL, 
-#                             ID = NULL,
-#                             FUN = "mean",
-#                             w = NULL,
-#                             join = TRUE) {
-#   .SD <-  NULL
-#   
-#   if(join & is.null(geom)){
-#     warning("Can't `join` to NULL geom")
-#     join = FALSE
-#   }
-#   
-#   w       = .find_w(file, geom, ID, w)
-#   w_names = c("grid_id", "w")
-#   ID      = names(w$weight_map)[!names(w$weight_map) %in% w_names]
-# 
-#   dt   = .zonal_io(file, w)
-#   cols = names(dt)[!names(dt) %in% c(ID, w_names)]
-#   
-#   if(FUN == 'mean'){
-#     FUN = function(x, w) { sum((x * w), na.rm = TRUE)  / sum(w, na.rm = TRUE)}
-#   } else if(FUN == "max"){
-#     FUN = function(x, w) { suppressWarnings(max(x, na.rm = TRUE)) }
-#   } else if(FUN == "min"){
-#     FUN = function(x, w) { suppressWarnings(min(x, na.rm = TRUE)) }
-#   } else if(FUN == "mode"){
-#     FUN = function(x, w) { suppressWarnings(getmode(x)) }
-#   } else if(FUN == "gm_mean"){
-#     FUN = function(x, w) { suppressWarnings(gm_mean(x)) }
-#   } else {
-#     stop("FUN not valid...")
-#   }
-#   
-#   threds = getDTthreads()
-#   setDTthreads(0)
-#     exe = dt[, lapply(.SD, FUN = FUN, w = w), keyby = eval(ID), .SDcols = cols]
-#   setDTthreads(threds)
-# 
-#   if(join){
-#     merge(geom, exe, by = ID)
-#   } else {
-#     exe
-#   }
-#   
-# }
+#' @importFrom terra nrow ncol xmin xmax ncell
 
 execute_zonal  = function(file = NULL, 
-                            geom = NULL, 
-                            ID = NULL,
-                            FUN = "mean",
-                            w = NULL,
-                            rcl = NULL,
-                            join = TRUE) {
+                          geom = NULL, 
+                          ID = NULL,
+                          FUN = "mean",
+                          w = NULL,
+                          rcl = NULL,
+                          join = TRUE) {
   
   . <- .SD <- frac_total <- NULL
   
-  if(join & is.null(geom)){
-    warning("Can't `join` to NULL geom")
-    join = FALSE
-  }
+  if(join & is.null(geom)){ join = FALSE }
   
-  w       = .find_w(file, geom, ID, w)
-  w_names = c("grid_id", "w")
-  ID      = names(w$weight_map)[!names(w$weight_map) %in% w_names]
-  
-  dt   = .zonal_io(file, w)
-  cols = names(dt)[!names(dt) %in% c(ID, w_names)]
-  
-
-  if(FUN == "freq"){
-      dt[, frac_total := (w / sum(w, na.rm = TRUE)), by = c(ID)]
-    
-      dt = dt[, .(freq = sum(frac_total, na.rm = TRUE)), by = c(ID, cols)]
-      
-      if(!is.null(rcl)){ 
-        dt[[cols]] = rcl$to[match(dt[[cols]], rcl$from)] 
-      }
-      
-      exe = setnames(dt, c(ID, "value", "percentage"))
-  } else {
-  
-    if(FUN == 'mean'){
-      FUN = function(x, w) { sum((x * w), na.rm = TRUE)  / sum(w, na.rm = TRUE)}
-    } else if(FUN == "max"){
-      FUN = function(x, w) { suppressWarnings(max(x, na.rm = TRUE)) }
-    } else if(FUN == "min"){
-      FUN = function(x, w) { suppressWarnings(min(x, na.rm = TRUE)) }
-    } else if(FUN == "mode"){
-      FUN = function(x, w) { suppressWarnings(getmode(x)) }
-    } else if(FUN == "gm_mean"){
-      FUN = function(x, w) { suppressWarnings(gm_mean(x)) }
-    } else {
-      stop("FUN not valid...")
+  if(inherits(file, 'list')){
+    #are they all the same?
+    if(any(length(unique(unlist(lapply(file, nrow)))) > 1,
+           length(unique(unlist(lapply(file, ncol)))) > 1,
+           length(unique(unlist(lapply(file, ncell)))) > 1,
+           length(unique(unlist(lapply(file, xmin)))) > 1,
+           length(unique(unlist(lapply(file, ymin)))) > 1)){
+      stop('list elements have different spatial properites: \nTry: `lapply`',
+           call.=FALSE)
     }
-    
-    exe = dt[, lapply(.SD, FUN = FUN, w = w), keyby = eval(ID), .SDcols = cols]
+  } else {
+    file = list(file)
   }
+  
+  w       = .find_w(file[[1]], geom, ID, w)
+  #w_names = c("grid_id", "w")
+  #ID      = names(w$weight_map)[!names(w$weight_map) %in% w_names]
+  
+  names    = names(file)
+
+  out = lapply(1:length(file), 
+         function(x){
+           single_file_execute(file = file[[x]], 
+                               w = w, 
+                               w_names = c("grid_id", "w"), 
+                               name = names[x], 
+                               FUN = FUN, 
+                               rcl = rcl, 
+                               ID = ID)
+         }
+  )
+  
+  exe <- Reduce(function(...) merge(..., all = TRUE, by = ID), out)
 
   if(join){
     merge(geom, exe, by = ID)
@@ -174,9 +164,4 @@ execute_zonal  = function(file = NULL,
     exe
   }
 }
-
-
-
-
-
 
